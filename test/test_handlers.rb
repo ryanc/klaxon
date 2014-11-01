21a2f6e5324fcadab3422341d668ee2b17c97bfa
Ryan Cavicchioni <ryan@cavi.cc>
2014-10-31 21:24:03 -0500

    Use a Twilio URL in the tests.

--- test/test_handlers.rb
+++ test/test_handlers.rb
@@ -9,7 +9,7 @@ class TestCallHandler < Minitest::Test
   end
 
   def test_greeting
-    post '/greeting', { 'Caller' => '+15555555555', 'RecordingUrl' => 'http://example.com/message.wav' }
+    post '/greeting', { 'Caller' => '+15555555555', 'RecordingUrl' => 'http://api.twilio.com/2010-04-01/Accounts/AC00000000000000000000000000000000/Recordings/RE00000000000000000000000000000000' }
     assert_includes(last_response.headers['Content-Type'], 'text/xml')
     assert_equal(200, last_response.status)
     assert_equal(
@@ -22,7 +22,7 @@ class TestCallHandler < Minitest::Test
     stub_request(:post, "https://events.pagerduty.com/generic/2010-04-15/create_event.json").
     to_return(:status => 200, :body => '{"status":"success","message":"Event processed","incident_key":"voicemail +15555555555"}')
 
-    post '/voicemail', { 'Caller' => '+15555555555', 'RecordingUrl' => 'http://example.com/message.wav' }
+    post '/voicemail', { 'Caller' => '+15555555555', 'RecordingUrl' => 'http://api.twilio.com/2010-04-01/Accounts/AC00000000000000000000000000000000/Recordings/RE00000000000000000000000000000000' }
     assert_includes(last_response.headers['Content-Type'], 'text/xml')
     assert_equal(
       %q(<?xml version="1.0" encoding="UTF-8"?><Response><Say>Thank you, a technician will be notified shortly.</Say><Hangup/></Response>),
