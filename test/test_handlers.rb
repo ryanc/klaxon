6656af0f08265d6e6b614a1f86bccbcc9b0971bd
Ryan Cavicchioni <ryan@cavi.cc>
2014-10-31 21:33:33 -0500

    DRY the recording_url sample.

--- test/test_handlers.rb
+++ test/test_handlers.rb
@@ -4,12 +4,16 @@ require 'webmock/minitest'
 class TestCallHandler < Minitest::Test
   include Rack::Test::Methods
 
+  def setup
+    @sample_recording_url = 'http://api.twilio.com/2010-04-01/Accounts/AC00000000000000000000000000000000/Recordings/RE00000000000000000000000000000000'
+  end
+
   def app
     CallHandler
   end
 
   def test_greeting
-    post '/greeting', { 'Caller' => '+15555555555', 'RecordingUrl' => 'http://api.twilio.com/2010-04-01/Accounts/AC00000000000000000000000000000000/Recordings/RE00000000000000000000000000000000' }
+    post '/greeting', { 'Caller' => '+15555555555', 'RecordingUrl' => @sample_recording_url }
     assert_includes(last_response.headers['Content-Type'], 'text/xml')
     assert_equal(200, last_response.status)
     assert_equal(
@@ -22,7 +26,7 @@ class TestCallHandler < Minitest::Test
     stub_request(:post, "https://events.pagerduty.com/generic/2010-04-15/create_event.json").
     to_return(:status => 200, :body => '{"status":"success","message":"Event processed","incident_key":"voicemail +15555555555"}')
 
-    post '/voicemail', { 'Caller' => '+15555555555', 'RecordingUrl' => 'http://api.twilio.com/2010-04-01/Accounts/AC00000000000000000000000000000000/Recordings/RE00000000000000000000000000000000' }
+    post '/voicemail', { 'Caller' => '+15555555555', 'RecordingUrl' => @sample_recording_url }
     assert_includes(last_response.headers['Content-Type'], 'text/xml')
     assert_equal(
       %q(<?xml version="1.0" encoding="UTF-8"?><Response><Say>Thank you, a technician will be notified shortly.</Say><Hangup/></Response>),
