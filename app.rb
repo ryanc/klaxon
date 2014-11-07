069a1415c5bfec78652eb52b2134de8a99616568
Ryan Cavicchioni <ryan@cavi.cc>
2014-11-06 20:23:18 -0600

    Move the PagerDutyGateway to a separate file.

--- app.rb
+++ app.rb
@@ -7,25 +7,7 @@ require 'sinatra/base'
 require 'pagerduty'
 require 'dotenv'
 
-class PagerDutyGateway
-  def initialize(service_key)
-    @pagerduty = Pagerduty.new(service_key)
-  end
-
-  def trigger_voicemail_event(caller, recording_url)
-    description = "New voicemail from #{caller}"
-    incident_key = "voicemail #{caller}"
-    details = { caller: caller, voicemail: recording_url }
-    @pagerduty.trigger(description, :incident_key => incident_key, :details => details)
-  end
-
-  def trigger_sms_event(caller, message)
-    description = "New SMS from #{caller}"
-    incident_key = "sms #{caller}"
-    details = { caller: caller, message: message }
-    @pagerduty.trigger(description, :incident_key => incident_key, :details => details)
-  end
-end
+require 'app/helpers/pagerduty'
 
 class Handler < Sinatra::Base
   configure do
